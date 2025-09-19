d18947a785abe580f8f1b9c779218e80c0c0b625
Pavel Khmara <375295560023pk@gmail.com>
2025-09-19 11:50:41 +0200

    app component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './styles/App.css';
+import PostsList from "./components/PostsList";
 
 function App() {
+    // const [ value, setValue ] = useState('Text your data');
+    const [ posts, setPosts ] = useState([
+      {id: 1, title: 'React app', body: 'description sd'},
+      {id: 2, title: 'React app sdf s', body: ' sdf description'},
+      {id: 3, title: 'React s df app', body: 'descri sdf sd fption'},
+      {id: 4, title: ' sdf sdReact app', body: 'descsdf sdf sfription'},
+    ])
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App container">
+        <PostsList posts={posts} />
+          {/* <h1>{value}</h1>
+          <input 
+            type="text"
+            value={value}
+            onChange={event => setValue(event.target.value)}
+            /> */}
+      </div>
   );
 }
 
